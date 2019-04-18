fbf159768049f9bad3389d1357e7e07721915f11
alephomega <johan@valuepotion.com>
2019-04-19 08:33:49 +0900

    CRM batch jobs

--- src/main/scala/com/kakaopage/global/crm/transformations/SummariesMerging.scala
+++ src/main/scala/com/kakaopage/global/crm/transformations/SummariesMerging.scala
@@ -1,37 +1,19 @@
 package com.kakaopage.global.crm.transformations
 
-import com.kakaopage.global.crm.{Aggregation, Distribution, Summary, Transformation}
+import com.kakaopage.global.crm._
 import com.typesafe.config.Config
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.{DataType, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class SummariesMerging(config: Config, spark: SparkSession) extends Transformation(config, spark) with Serializable {
 
   override def transform(dataFrames: DataFrame*): DataFrame = {
-    val rdd: RDD[Row] = dataFrames(0).rdd
-    rdd.map(toSummary)
-      .groupBy(s => (s.customer, s.event))
-      .map {
-        case ((customer: String, event: String), summaries: Iterable[Summary]) => {
-          val aggregation = merge(summaries)
-          Row.merge(Seq[Row](Row(customer, event), aggregation.toRow): _*)
-        }
-      }
-
-      spark.createDataFrame(rdd, schema).repartition(config.getInt("sink.partitions"))
+    dataFrames(0)
+      .groupBy(col("customer"), col("event"))
+      .agg(Merger(config.getString("service.timezone")).toColumn.alias("summary"))
+      .select(col("customer"), col("event"), col("summary.last").alias("last"), col("summary.frequency").alias("frequency"), col("summary.distribution").alias("distribution"))
+      .repartition(config.getInt("sink.partitions"))
   }
-
-  def merge(summaries: Iterable[Summary]): Aggregation = {
-    summaries.foldLeft(Aggregation(config.getString("service.timezone"))) ((aggregation, summary) => Aggregation.aggregate(aggregation, summary))
-  }
-
-  def toSummary(row: Row): Summary = {
-    val distribution = row.getAs[Row]("distribution")
-    Summary(row.getAs[String]("customer"), row.getAs[String]("event"), row.getAs[String]("last"), row.getAs[Int]("frequency"), Distribution(distribution.getAs[Array[Int]]("hour"), distribution.getAs[Array[Int]]("day")))
-  }
-
-  val schema: StructType = DataType.fromJson(config.getString("sink.schema")).asInstanceOf[StructType]
 }
 
 
